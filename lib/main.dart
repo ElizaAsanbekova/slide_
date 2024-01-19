487e5f3ce50caefba3a5350620c96a5977a63bed
Elizaasanbekova01 <asanbekovaeliza01@gmail.com>
2024-01-20 01:08:27 +0300

    Slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyWay());
+  runApp(MyApp());
 }
 
-class MyWay extends StatelessWidget {
-  MyWay({super.key});
-  int kel = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +15,6 @@ class MyWay extends StatelessWidget {
   }
 }
 
-int kel = 0;
-
 class MyClass extends StatefulWidget {
   const MyClass({super.key});
 
@@ -25,58 +22,78 @@ class MyClass extends StatefulWidget {
   State<MyClass> createState() => _MyClassState();
 }
 
+int index = 0;
+
 List<String> surottor = [
-  'butterfly.jpg'
-      'foto.jpg'
-      'laterne.jpg'
-      'lily.jpg'
-      'mountain,jpg'
+  'butterfly.jpg',
+  'foto.jpg',
+  'laterne.jpg',
+  'lily.jpg',
+  'mountain.jpg',
+  'laterne.jpg',
 ];
+
 
 class _MyClassState extends State<MyClass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Task01")),
+        title: const Center(child: Text("Task01")),
       ),
       body: Center(
         child: Column(
           children: [
-            Text(
+           
+            const Text(
               "slider",
               style: TextStyle(fontSize: 25),
             ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(0),
+                child: Image.asset(
+                  "assets/images/${surottor[index]}",
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 InkWell(
                   onTap: () {
                     setState(() {
-                      print('goback');
+                      index = index + 1;
+                      if (surottor.length == index) {
+                        index = 0;
+                      }
+                      print(surottor.length);
                     });
                   },
                   child: Container(
                     child: Icon(Icons.arrow_back_ios),
                   ),
                 ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      if (index == 0) {
+                        index = surottor.length - 1;
+                      } else {
+                        index--;
+                      }
+                      print('goforward');
+                    });
+                  },
+                  child: Container(
+                    child: Icon(Icons.arrow_forward_ios),
+                  ),
+                ),
               ],
-            ),
-            Container(
-              child: Image.asset(
-                "assets/${surottor[kel]}",
-                height: 200,
-                width: 200,
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  print('goforward');
-                });
-              },
-              child: Container(
-                child: Icon(Icons.arrow_forward_ios),
-              ),
             ),
           ],
         ),
